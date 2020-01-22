c192c1575336002de5ac05107e80c0de500ff420
Matt <matt@example.com>
2020-01-22 21:20:11 +0000

    Fix when not in root

--- pwatest/sw.js
+++ pwatest/sw.js
@@ -3,7 +3,7 @@ var cacheName = 'test app';
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
-    caches.open(cacheName).then(function(cache) { return cache.addAll(["/", "/index.html", "/test.js", "/sw.js", "/test.manifest", "/images/test-icon-32.png", "/images/test-icon-128.png", "/images/test-icon-144.png", "/images/test-icon-152.png", "/images/test-icon-167.png", "/images/test-icon-180.png", "/images/test-icon-192.png", "/images/test-icon-256.png", "/images/test-icon-512.png", "/favicon-32.png"]); })
+    caches.open(cacheName).then(function(cache) { return cache.addAll(["index.html", "test.js", "sw.js", "test.manifest", "images/test-icon-32.png", "images/test-icon-128.png", "images/test-icon-144.png", "images/test-icon-152.png", "images/test-icon-167.png", "images/test-icon-180.png", "images/test-icon-192.png", "images/test-icon-256.png", "images/test-icon-512.png", "favicon-32.png"]); })
     );
 });
 
