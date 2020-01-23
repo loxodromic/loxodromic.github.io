9f545734a516962b93e29339b2ef8f8412776044
Matt <matt@example.com>
2020-01-23 23:27:15 +0000

    Updated test, output from latest pwap8

--- pwatest/sw.js
+++ pwatest/sw.js
@@ -1,4 +1,6 @@
 //sw.js...
+//see https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
+
 var cacheName = 'test app';
 
 self.addEventListener('install', function(event) {
